6304433e5de3fce0011f8c220862b5c2075464c8
nabtodaemon <panneer.selvam@daemon.co.in>
2017-11-01 18:34:02 +0530

    Update unabto_platform_types.h

--- src/unabto_platform_types.h
+++ src/unabto_platform_types.h
@@ -1,8 +1,14 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include <stddef.h>
 #include <time.h>
 
+#ifdef __MACH__
+#include <mach/clock.h>
+#include <mach/mach.h>
+#endif
+
+typedef int ssize_t;
 typedef uint64_t nabto_stamp_t;
 typedef int64_t nabto_stamp_diff_t;
-
 typedef int nabto_socket_t;
