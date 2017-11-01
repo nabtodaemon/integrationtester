55645ad97e1f04083703714a77f210b726a2e4e6
nabtodaemon <panneer.selvam@daemon.co.in>
2017-11-01 18:35:17 +0530

    Update unabto_time_adapter_test.c

--- src/unabto_time_adapter_test.c
+++ src/unabto_time_adapter_test.c
@@ -2,7 +2,19 @@
 
 nabto_stamp_t nabtoGetStamp() {
     struct timespec res;
+#ifdef __MACH__
+    clock_serv_t cclock;
+    mach_timespec_t mts;
+    host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+    clock_get_time(cclock, &mts);
+    mach_port_deallocate(mach_task_self(), cclock);
+    res.tv_sec = mts.tv_sec;
+    res.tv_nsec = mts.tv_nsec;
+#elif defined(CLOCK_MONOTONIC)
     clock_gettime(CLOCK_MONOTONIC, &res);
+#else
+    clock_gettime(CLOCK_REALTIME, &res);
+#endif
     return res.tv_sec*1000 + (res.tv_nsec/1000000);
 }
 
