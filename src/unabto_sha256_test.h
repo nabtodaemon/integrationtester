d991cd2aa7e42259ddb755d6d82a469fd49ae7e9
nabtodaemon <panneer.selvam@daemon.co.in>
2017-12-08 18:30:21 +0530

    Update unabto_sha256_test.h

--- src/unabto_sha256_test.h
+++ src/unabto_sha256_test.h
@@ -3,7 +3,9 @@
 
 void unabto_sha256(const uint8_t* message, size_t message_len, unsigned char* digest);
 
+#ifndef UNABTO_CRYPTO_MODULE_LIBTOMCRYPT
 bool sha256_test(void);
+#endif
 
 int sha256_timing_test(void);
 
